406e17b97d9e3a0c77f7926b495fec5e4bed0b6c
benpiticaru <benpiticaru@gmail.com>
2022-12-16 15:40:29 -0500

    Update

---
+++ Music Scheduling/Scheduling.py
@@ -0,0 +1,132 @@
+import numpy as np
+import pandas as pd
+from datetime import date
+import calendar
+from itertools import cycle, islice
+
+
+
+address = 'C:/Users/Benjamin Piticaru/Downloads/January - April Leading survey.csv'
+survey_df = pd.read_csv(address)
+survey_df = survey_df.drop('Timestamp', axis=1)
+
+survey_df.columns = ['Name','email','phone','role','Zions Harp',
+                    'Gospel Hymns','Hymns Zion','Celebration Book',
+                    'Junior Hymnal', 'Camp Book','Dates off','Capacity','Groupchat']
+
+## Changing Inputs from Yes/No and capacity to bool and 3,2,1 respecively
+survey_df.replace(('Yes', 'No'), (True, False), inplace=True)
+survey_df['Capacity'].replace(('regular (>3/month)', 'occasional (1-2/month)', 'reserve (0/month)'), (3, 2, 0), inplace=True)
+survey_df.head()
+
+## Creating the lists of Capacity per month
+Leader_list = []
+Piano_list = []
+
+def multi_append(name, n):
+    l = []
+    for i in range(n):
+        l.append(name)
+    return l
+
+for i in range(len(survey_df)):
+    if survey_df['role'][i] == 'Leader':
+        Leader_list.extend(multi_append(survey_df['Name'][i], survey_df['Capacity'][i]))
+    else:
+        Piano_list.extend(multi_append(survey_df['Name'][i], survey_df['Capacity'][i]))
+
+## Inputs the dates from January to April
+timespan = 120
+begin_date = '2023-01-01'
+df = pd.DataFrame({'Start Date':pd.date_range(begin_date, periods=timespan),
+                   'End Date':pd.date_range(begin_date, periods=timespan)})
+
+## Filters out rows that are not wednesdays or Sundays
+for index, row in df.iterrows():
+    if row['Start Date'].weekday() == 6:
+        continue
+    elif row['Start Date'].weekday() == 2:
+        continue
+    else:
+        df.drop(index, inplace=True)
+
+## Creates Additional Sunday placement.
+temp_df = []
+for index, row in df.iterrows():
+    if row['Start Date'].weekday() == 6:
+        temp_df.extend([list(row)]*2)
+    else:
+        temp_df.append(list(row))
+
+df = pd.DataFrame(temp_df, columns=df.columns)
+
+## Implementing Sit/Stand format
+def format(row):
+    if row['Start Date'].weekday() == 6:
+        return 'Sit'
+    else:
+        return    
+
+df['format'] = df.apply(lambda row: format(row), axis=1)
+
+if df['Start Date'][0].weekday() == 6:
+    df['format'][0] = None
+
+for i in range(3, len(df) - 1):
+    if df['format'][i-2] != None:
+        df['format'][i] = None
+    elif df['format'][i-3] == 'Sit':
+        df['format'][i] = 'Stand'
+    else:
+        continue
+
+## Adding start and stop times
+def start_time_of_day(row):
+    if row['Start Date'].weekday() == 6:
+        if row['format'] == None:
+            return '4:40:00 PM'
+        else:
+            return '5:15:00 PM'
+    else:
+        return '7:30:00 PM'
+
+def end_time_of_day(row):
+    if row['Start Date'].weekday() == 6:
+        if row['format'] == None:
+            return '5:15:00 PM'
+        else:
+            return '5:45:00 PM'
+    else:
+        return '7:55:00 PM'
+
+df['Start Time'] = df.apply(lambda row: start_time_of_day(row), axis=1)
+df['End Time'] = df.apply(lambda row: end_time_of_day(row), axis=1)
+
+## Adding Book used
+book_list = ['Zions Harp','Celebration Hymnal',
+'Gospel Hymns','Zions Harp','Gospel Hymns','Zions Harp',
+'Zions Harp','Gospel Hymns','Gospel Hymns','Zions Harp',
+'Hymns of Zion','Zions Harp','Zions Harp','Junior Hymnal',
+'Gospel Hymns','Zions Harp','Gospel Hymns','Zions Harp',
+'Zions Harp','Gospel Hymns','Gospel Hymns','Zions Harp',
+'Camp Book','Zions Harp']
+
+if df['Start Date'][0].weekday() == 2:
+    book_list.insert(0,'Zions Harp')
+
+df['Subject'] = list(islice(cycle(book_list), len(df)))
+
+## Need to divide df into months to have stratified randomization
+
+## Need to make a dataframe that holds dates that people can't lead (df with peoples names as columns and dates as 
+#   indexes and binary values (yes no, true false))
+#       will have to figure out a way to change input dates to numeric dates
+
+## The function below takes a list and returns a random value from the list without replacing it.
+def rd_wo_replacement(los):
+    x = np.random.choice(los)
+    for i in range(len(los)):
+        if (los[i] == x):
+            los.remove(los[i])
+            return x
+
